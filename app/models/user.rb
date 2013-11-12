091bfb210d132ab88e9be41ce8f8b68c7080f304
Halfdan Rump <dalekk@gmail.com>
2013-11-12 16:55:58 +0900

    Adds password_digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	before_save {self.email.downcase!}
 	validates :name, presence: true, length: {minimum: 3, maximum: 50}
 	validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: {case_sensitive: false}
 end
